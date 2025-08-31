fd131b19eb8a7fc3346a5f165f271e8635ad1e21
zahraashoaib2002-maker <zahraashoaib2002@gmail.com>
2025-08-31 19:59:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 function openWhatsApp(e){
   e?.preventDefault?.();
-  const phone="96170796137";
-  const appUrl=`whatsapp://send?phone=${phone}`;
-  const webUrl=`https://wa.me/${phone}`;
-  window.location.href=appUrl;
-  setTimeout(()=>{window.location.href=webUrl},800);
+  const phone = "96170796137";
+  const appUrl = `whatsapp://send?phone=${phone}`;
+  const webUrl = `https://wa.me/${phone}`;
+  const t = Date.now();
+  window.location.href = appUrl;
+  setTimeout(()=>{ if(Date.now()-t<1500) window.location.href = webUrl; }, 800);
 }
 function openInstagram(e){
   e?.preventDefault?.();
-  const user="tafawok_edu_center.lb";
-  const appUrl=`instagram://user?username=${user}`;
-  const webUrl="https://www.instagram.com/tafawok_edu_center.lb";
-  window.location.href=appUrl;
-  setTimeout(()=>{window.location.href=webUrl},800);
+  const user = "tafawok_edu_center.lb";
+  const appUrl = `instagram://user?username=${user}`;
+  const webUrl = "https://www.instagram.com/tafawok_edu_center.lb?igsh=dnN0eXN3YXQzcHEz&utm_source=qr";
+  const t = Date.now();
+  window.location.href = appUrl;
+  setTimeout(()=>{ if(Date.now()-t<1500) window.location.href = webUrl; }, 800);
 }
